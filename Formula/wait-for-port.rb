7a213bf1bd7453ead393e30fac54435c2147b2e5
Charles Srisuwananukorn <csrisuw@gmail.com>
2024-04-20 19:49:26 -0700

    wait-for-port: add formula for wait-for-port

---
+++ Formula/wait-for-port.rb
@@ -0,0 +1,22 @@
+class WaitForPort < Formula
+  desc "wait-for-port waits for a host to listen on a port"
+  homepage "https://github.com/csris/wait-for-port"
+  url "https://github.com/csris/wait-for-port/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "40dfe0323817b12e81eac55d41962cfa21e86b85ccd46a436f56d81aacc92d3a"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+  uses_from_macos "nc"
+
+  def install
+    args = %W[
+      prefix=#{prefix}
+    ]
+
+    system "make", *args, "install"
+  end
+
+  test do
+    system "#{bin}/wait-for-port", "-p", "443", "www.google.com"
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# homebrew-tap
+# Csris Tap
+
+## How do I install these formulae?
+
+`brew install csris/tap/<formula>`
+
+Or `brew tap csris/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
